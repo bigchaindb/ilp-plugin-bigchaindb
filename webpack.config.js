9682849c489cb3a6364d2658d46eda5f68546ea0
diminator <dimi.dejonghe@gmail.com>
2016-06-16 14:58:20 +0200

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+/* eslint-disable strict, no-console, object-shorthand */
+/* eslint-disable import/no-extraneous-dependencies, import/newline-after-import */
+'use strict';
+
+const path = require('path');
+
+const webpack = require('webpack');
+const combineLoaders = require('webpack-combine-loaders');
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const PATHS = {
+    ILP_PLUGIN: path.resolve(__dirname, 'src/lib/bigchaindb_ledger_plugin.js'),
+
+    BUILD: path.resolve(__dirname, 'build'),
+    DIST: path.resolve(__dirname, 'dist'),
+    NODE_MODULES: path.resolve(__dirname, 'node_modules'),
+};
+
+/** EXTERNAL DEFINITIONS INJECTED INTO APP **/
+const DEFINITIONS = {
+    'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    },
+};
+
+
+/** PLUGINS **/
+const PLUGINS = [
+    new webpack.DefinePlugin(DEFINITIONS),
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+        Promise: 'imports?this=>global!exports?global.Promise!es6-promise',
+        fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
+];
+
+const PROD_PLUGINS = [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        output: {
+            comments: false
+        }
+    }),
+    new webpack.LoaderOptionsPlugin({
+        debug: false,
+        minimize: true
+    }),
+];
+
+
+if (PRODUCTION) {
+    PLUGINS.push(...PROD_PLUGINS);
+}
+
+
+/** LOADERS **/
+const JS_LOADER = combineLoaders([
+    {
+        loader: 'babel',
+        query: {
+            cacheDirectory: true,
+        },
+    },
+]);
+
+
+const LOADERS = [
+    {
+        test: /\.jsx?$/,
+        exclude: [PATHS.NODE_MODULES],
+        loader: JS_LOADER,
+    },
+];
+
+
+/** EXPORTED WEBPACK CONFIG **/
+module.exports = {
+    entry: PATHS.ILP_PLUGIN,
+
+    output: {
+        filename: PRODUCTION ? '[name]/bundle.min.js' : '[name]/bundle.js',
+        path: PRODUCTION ? PATHS.DIST : PATHS.BUILD,
+    },
+
+    debug: !PRODUCTION,
+
+    devtool: PRODUCTION ? '#source-map' : '#inline-source-map',
+
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        modules: ['node_modules'], // Don't use absolute path here to allow recursive matching
+    },
+
+    plugins: PLUGINS,
+
+    module: {
+        loaders: LOADERS,
+    },
+};
